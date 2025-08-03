169eb82faf635acc8934212dd6987ddb2aaec637
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2025-08-04 00:32:43 +0200

    feat: remove single quote from auto pair for rust

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -13,3 +13,30 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
     vim.opt.ft = "hyprlang"
   end,
 })
+
+local rust_group = vim.api.nvim_create_augroup("Rust_disable_single_quote", { clear = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "rust",
+  group = rust_group,
+  callback = function()
+    if MiniPairs ~= nil then
+      MiniPairs.unmap("i", "'", "''")
+    end
+  end,
+  desc = "Disable single quote Rust",
+})
+
+vim.api.nvim_create_autocmd("User", {
+  pattern = "LazyLoad",
+  group = vim.api.nvim_create_augroup("Rust_disable_single_quote_lazyload", { clear = true }),
+  callback = function(event)
+    if event.data == "mini.pairs" then
+      vim.api.nvim_exec_autocmds("FileType", {
+        pattern = "rust",
+        group = rust_group,
+      })
+    end
+  end,
+  desc = "Disable single quote Rust",
+})
