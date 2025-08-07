55d128ebdc1e53a5d3ff5d58df5e2ae2292bdb3f
Lorenzo Rapetti <lorenzo.rapetti.94@gmail.com>
2025-08-07 16:17:17 +0200

    feat: add qmk.nvim to nvim

---
+++ .config/nvim/lua/plugins/qmk.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "codethread/qmk.nvim",
+    opts = {
+      name = "LAYOUT_split_3x6_3",
+      variant = "qmk",
+      layout = {
+        "x x x x x x _ _ _ x x x x x x",
+        "x x x x x x _ _ _ x x x x x x",
+        "x x x x x x _ _ _ x x x x x x",
+        "_ _ _ _ x x x _ x x x _ _ _ _",
+      },
+    },
+  },
+}
